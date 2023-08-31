aa61a7681d548235993b0e8012a67cf92301cfc5
TANMMAY24 <dixittany@gmail.com>
2023-08-31 20:47:03 +0530

    update

--- server.py
+++ server.py
@@ -5,15 +5,45 @@ app = Flask(__name__)
 
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
-    return ''
+    return "Hello world!"
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
-    return ''
+    """
+    Add two given numbers.
+
+    Parameters:
+        first (number): The first number to add.
+        second (number): The second number to add.
+
+    Returns:
+        JSON object: A JSON object with the following properties:
+            result (number): The result of the summation.
+    """
+    data = json.loads(request.data)
+    first = data['first']
+    second = data['second']
+    result = first + second
+    return json.dumps({'result': result})
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
-    return ''
+    """
+    Subtract two given numbers.
+
+    Parameters:
+        first (number): The first number to subtract.
+        second (number): The second number to subtract.
+
+    Returns:
+        JSON object: A JSON object with the following properties:
+            result (number): The result of the subtraction.
+    """
+    data = json.loads(request.data)
+    first = data['first']
+    second = data['second']
+    result = first - second
+    return json.dumps({'result': result})
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
